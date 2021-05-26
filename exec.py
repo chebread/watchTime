88566e7ded7f3c03da7ff74a3b2441530e80adf3
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-05-26 20:06:10 +0900

    version: 2

--- exec.py
+++ exec.py
@@ -1,6 +1,35 @@
-# 고전 시계
+# 시계
 from datetime import *
-print("-- 시계 --")
-clock = datetime.now() # Flame
-watch = "%s년 %s월 %s일 %s시 %s분 %s초"%(clock.year, clock.month, clock.day, clock.hour, clock.minute, clock.second)
+def Time():
+    _time_ = datetime.now()
+    day = _time_.day
+    month = _time_.month
+    leap_year = LeapYear(_time_.year)
+    year = _time_.year
+    second = _time_.second
+    minute = _time_.minute
+    hour = AM(_time_.hour)
+    return "%s년 %s월 %s일 %s시 %s분 %s초"%(year, month, day, hour, minute, second)
+def AM(hour):
+    if hour > 12:
+        am = hour - 12
+        return am
+def LeapYear(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return 1
+    else:
+        return -1
+def DisplayLeapYear(n):
+    if n == -1:
+        return "올해는 윤년이에요"
+    else:
+        pass
+watch = Time()
+print("Watch")
+_time_ = datetime.now()
+if LeapYear(_time_.year) == 1:
+    print(DisplayLeapYear(LeapYear(_time_.year)))
+else:
+    pass
 print(watch)
+print("POWER BY HE")
